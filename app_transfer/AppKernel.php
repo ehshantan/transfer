4f3a5c0295c796d9287e8efb48305580e3ff9b6e
ehshantan <ehs.hantan@lgsconnect.com>
2014-04-28 07:25:19 +0700

    add CAD Transfer Bundle

--- app_transfer/AppKernel.php
+++ app_transfer/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Propel\PropelBundle\PropelBundle(),
+            new CAD\TransferBundle\CADTransferBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src_transfer/CAD/TransferBundle/CADTransferBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CAD\TransferBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CADTransferBundle extends Bundle
+{
+}
